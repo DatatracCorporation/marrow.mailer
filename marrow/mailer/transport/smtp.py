f252be7ec96ff075cc9f389f4f6909450da5dc81
Jim Patterson <jpatterson@datatrac.com>
2015-04-01 12:11:31 -0400

    Catch errors with encoding during smtp send

--- marrow/mailer/transport/smtp.py
+++ marrow/mailer/transport/smtp.py
@@ -109,11 +109,11 @@ class SMTPTransport(object):
 				raise TransportExhaustedException()
 	
 	def send_with_smtp(self, message):
-		sender = bytes(message.envelope)
-		recipients = message.recipients.string_addresses
-		content = bytes(message)
-		
 		try:
+			sender = bytes(message.envelope)
+			recipients = message.recipients.string_addresses
+			content = bytes(message)
+		
 			self.connection.sendmail(sender, recipients, content)
 			self.sent += 1
 		
